68b8b15d383944e96b38ccf380aac0a5893dbb76
Yoshimasa Komatsubara <hojokaban@gmail.com>
2020-08-09 17:17:57 +0900

    Check the movement

--- src/practice_eclipse/PracticeEclipse.java
+++ src/practice_eclipse/PracticeEclipse.java
@@ -2,6 +2,6 @@ package practice_eclipse;
 
 public class PracticeEclipse {
 	public static void main(String[] args) {
-		System.out.println("Hello Eclipse! Let me use Java more easily!");
+		System.out.println("Hello Eclipse! Let me use Java more easily!!");
 	}
 }
